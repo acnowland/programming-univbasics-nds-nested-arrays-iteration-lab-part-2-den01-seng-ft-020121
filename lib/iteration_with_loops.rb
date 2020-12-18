156b46eb3488973022a26e13f45ae848a4f218d4
Adam Nowland <acnowland@gmail.com>
2020-12-18 21:33:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,10 @@ def find_min_in_nested_arrays(src)
   while outer_array_index < src.length do 
     inner_array_index = 0 
     min_daily_temp = src[outer_array_index][0]
-    puts src[outer_array_index].length
+    while inner_array_index < src[outer_array_index].length do 
+      puts inner_array_index
+      inner_array_index += 1 
+    end
     outer_array_index += 1
   end
 end
